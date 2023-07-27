383a0e616fd46772c35252601f7a7f4e1eb0bde4
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-07-27 10:25:34 -0500

    iniciando repo de node.js

---
+++ app.js
@@ -0,0 +1,6 @@
+/*
+terminal> node app (no es necesario agregar .js ya que node lo sobreentiende)
+*/
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+numbers.forEach((n) => console.log(n * 2));
