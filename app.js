7c8b0312e02cdfb2af09a6192018c26acc2b589d
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-07-27 10:33:14 -0500

    agregando package.json

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ terminal> node app (no es necesario agregar .js ya que node lo sobreentiende)
 const numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
 numbers.forEach((n) => console.log(n * 2));
+
+/*
+Sistema de versionados = semantic versioning
+MAJOR.MINOR.PATH
+ejm: 4.5.2
+*/
