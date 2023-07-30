419bcc111b9ebba927803ef95392e598a955cb92
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-07-30 18:09:43 -0500

    enviando archivhos estáticos, creando página 404

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
-import express from 'express';
-import router from './routes/index.routes.js'
+import express from "express";
+import router from "./routes/index.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Servidor a la espera de conexiones');
-})
+//Creando servidor
+app.listen(3000, () => {
+  console.log("Servidor a la espera de conexiones");
+});
 
 const rutas = router;
 
 //Rutas
 app.use(rutas);
-//app.use(require('./routes/index.routes'))
+
+//Static files
+app.use(express.static(path.join(__dirname,'../public')))
+console.log(__dirname);
+console.log(path.join(__dirname,'../public'));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname,"../public/index.html"));
+});
